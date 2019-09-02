cb5e5161fc644a64ac298be9c790bd0656c05b07
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-02 11:36:28 +0300

    registering controllers in container

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controllers;
 
+use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class HomeController
 {
+    protected $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
     public function index(Request $request, Response $response)
     {
-        $response->getBody()->write("home");
-
-        return $response;
+        return $this->container->get('view')->render($response, 'home.twig');
     }
 }
--- bootstrap/container.php
+++ bootstrap/container.php
@@ -2,7 +2,11 @@
 
 use DI\Container;
 use Slim\Factory\AppFactory;
+use App\Controllers\HomeController;
 
 $container = new Container();
-
 AppFactory::setContainer($container);
+
+$container->set(HomeController::class, function ($container) {
+    return new HomeController($container);
+});
