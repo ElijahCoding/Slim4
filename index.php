e21af9b24144b4be39c8e1fb90682e9e70db22f9
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-02 08:54:39 +0300

    error handling middleware

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Slim\Factory\AppFactory;
+use Slim\Middleware\ErrorMiddleware;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -8,10 +9,20 @@ require 'vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+$errorMiddleware = new ErrorMiddleware(
+    $app->getCallableResolver(),
+    $app->getResponseFactory(),
+    true,
+    false,
+    false
+);
+
+$app->add($errorMiddleware);
+
+// $app->get('/', function (Request $request, Response $response, $args) {
+//     $response->getBody()->write("Hello world!");
+//     return $response;
+// });
 
 
 $app->run();
